5165b4b2796b54371897c9159c988650694e8fa1
ameyliasandy <ameyliasandyputriazzahra@gmail.com>
2025-05-06 21:42:41 +0700

    update praktikum

---
+++ resources/views/ameylia.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Gambar</title>
+    <link rel="stylesheet" href="styles/style.css">
+
+
+</head>
+<body>
+    <h1>Gambar Mobil</h1>
+    <img src="/images/rush.jpg" alt="Mobil 1">
+    <img src="/images/toyotafortuner.jpg" alt="Mobil 2">
+
+</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,26 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-     Selamat datang di home {{$nama}}<br>
-     Anda adalah seorang {{$pekerjaan}}
-     
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Gambar</title>
+    <script src="https://cdn.tailwindcss.com/3.4.1"></script>
+    
+
+
+</head>
+
+
+<body>
+<div class="bg-green-200 p-6 m-4 rounded">
+    <p class="text-gray-700">Ini adalah box dengan Tailwind CSS</p>
 </div>
+
+</head>
+
+    <h1>Gambar Mobil</h1>
+
+    <img src="images/rush.jpg" alt="Mobil 1">
+    <img src="images/toyotafortuner.jpg" alt="Mobil 2">
+
+</body>
+</html>
